0659cf52b92238adfafdcbe75a1fb4aea5a7b8b7
Tamal Saha <tamal@appscode.com>
2022-09-23 22:41:21 -0700

    Use Go 1.19 (#3)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"reflect"
@@ -131,7 +130,7 @@ func Generate(dir string, gvk schema.GroupVersionKind, s interface{}) {
 
 	path := filepath.Join(dir, rid.Group, rid.Version)
 	_ = os.MkdirAll(path, 0755)
-	_ = ioutil.WriteFile(filepath.Join(path, filename), buf.Bytes(), 0644)
+	_ = os.WriteFile(filepath.Join(path, filename), buf.Bytes(), 0644)
 }
 
 func IsOfficialType(group string) bool {
